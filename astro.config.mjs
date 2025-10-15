2af44f12a08c40b312cceb86d38fe8f7df6c35e2
giohering <gio.hering@gmail.com>
2025-10-15 16:34:31 -0400

    Update astro.config.mjs to have math

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,15 +4,27 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import react from '@astrojs/react';
 import tinaDirective from "./astro-tina-directive/register"
-
-import { defineConfig } from 'astro/config';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 
 
 // https://astro.build/config
 export default defineConfig({
 	site: process.env.SITE_URL || `https://${process.env.VERCEL_URL}`,
 	integrations: [mdx(), sitemap(), react(), tinaDirective()],
-	  markdown: {
+		markdown: { 
+			remarkPlugins: [
+		      remarkMath,
+		      remarkToc,
+		      [remarkCollapse, { test: "Table of contents" }],
+		    ],
+			rehypePlugins: [rehypeKatex],
+			  shikiConfig: {
+		      // For more themes, visit https://shiki.style/themes
+		      themes: { light: "min-light", dark: "night-owl" },
+		      wrap: false,
+		    },
+  },
     shikiConfig: {
       themes: {
         light: 'github-light',
